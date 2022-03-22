799d75225e86a160f809bf0dbd15206ae7d4eb09
Ben Fitzhardinge <ben@fitzhardinge.net>
2022-03-22 12:53:33 +0800

    flake8 compatible

--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -1,5 +1,4 @@
 from django.http import Http404
-from django.shortcuts import render
 from rest_framework import permissions, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
--- crowdfunding/users/views.py
+++ crowdfunding/users/views.py
@@ -1,5 +1,4 @@
 from django.http import Http404
-from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
